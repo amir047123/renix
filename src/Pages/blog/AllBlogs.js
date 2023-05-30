f584764c0c6dea38d24e2086a29d802764155367
ifty03 <ashikulislamb84@gmail.com>
2023-05-30 21:56:59 +0600

    all blog page added

--- src/Pages/blog/AllBlogs.js
+++ src/Pages/blog/AllBlogs.js
@@ -1,155 +1,153 @@
-import React from 'react'
-import { CiSearch } from 'react-icons/ci'
-import { RiDeleteBin6Line } from 'react-icons/ri'
-import { TbEdit } from 'react-icons/tb'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { CiSearch } from "react-icons/ci";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { TbEdit } from "react-icons/tb";
+import Swal from "sweetalert2";
+import Pagination from "../../shared/Pagination/Pagination";
 const AllBlogs = () => {
-    return (
-        <section className="py-10 md:py-14">
-            <div className="container px-6 md:max-w-6xl w-full ">
-                {/* search bar */}
+  const [blogs, setBlogs] = useState([]);
+  // for pagination
+  const [quantity, setQuantity] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(6);
+  const [refresh, setRefresh] = useState(false);
+  useEffect(() => {
+    const url = `http://localhost:5000/api/v1/blogs?size=${size}&page=${page}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlogs(data?.data);
+        setQuantity(data?.total);
+      });
+  }, [size, page, refresh]);
 
-                <form className="flex items-center justify-end text-right gap-3 mb-6">
-                    <label for="simple-search" className="text-sm text-textColor">Search</label>
-                    <div className="relative ">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <span className="text-xl text-textColor">
-                                <CiSearch />
-                            </span>
-                        </div>
-                        <input type="text" className="bg-[#F0FDF4] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-2.5 py-3 border-none" placeholder="Search" required />
-                    </div>
+  const handelDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/api/v1/blogs/deleteBlog/${id}`, {
+          method: "DELETE",
+        }).then((res) => {
+          if (res.status === 200) {
+            setRefresh(!refresh);
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          }
+        });
+      }
+    });
+  };
+  return (
+    <section className="py-10 md:py-14">
+      <div className="container px-6 md:max-w-6xl w-full ">
+        {/* search bar */}
 
-                </form>
-
-                {/* medicine list table */}
-                <div className="relative overflow-x-auto">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border border-[#d0d2dadd]  table_head rounded-lg">
-                            <tr className='py-4 rounded-lg'>
-                                <th scope="col" className="px-6 py-3  text-[13px] font-medium capitalize">
-                                    Seriol No
-                                </th>
-
-                                <th scope="col" className="px-6 py-3  text-[13px] font-medium capitalize">
-                                    Title
-                                </th>
-                                <th scope="col" className="px-6 py-3  text-[13px] font-medium capitalize">
-                                    Category
-                                </th>
-
-                                <th scope="col" className="px-6 py-3  text-[13px] font-medium capitalize">
-                                    Action
-                                </th>
-                            </tr>
-
-
-
-                        </thead>
-                        <tbody>
-                            <tr className="bg-white border-b border-[#D0D2DA]">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    01
-                                </th>
-                                <td className="px-6 py-4">
-                                    Lorem ipsum dolor sit imet
-                                </td>
-                                <td className="px-6 py-4">
-                                    International
-                                </td>
-
-                                <td className="px-6 py-4">
-                                    <span className="flex items-center gap-3">
-
-
-                                        <button className="text-lg text-[#0077FF] bg-[#BBDDFF] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <TbEdit />
-                                        </button>
-                                        <button className="text-lg text-[#F87171] bg-[#FEE2E2] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <RiDeleteBin6Line />
-                                        </button>
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b border-[#D0D2DA]">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    01
-                                </th>
-                                <td className="px-6 py-4">
-                                    Lorem ipsum dolor sit imet
-                                </td>
-                                <td className="px-6 py-4">
-                                    International
-                                </td>
-
-                                <td className="px-6 py-4">
-                                    <span className="flex items-center gap-3">
-
-
-                                        <button className="text-lg text-[#0077FF] bg-[#BBDDFF] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <TbEdit />
-                                        </button>
-                                        <button className="text-lg text-[#F87171] bg-[#FEE2E2] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <RiDeleteBin6Line />
-                                        </button>
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b border-[#D0D2DA]">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    01
-                                </th>
-                                <td className="px-6 py-4">
-                                    Lorem ipsum dolor sit imet
-                                </td>
-                                <td className="px-6 py-4">
-                                    International
-                                </td>
-
-                                <td className="px-6 py-4">
-                                    <span className="flex items-center gap-3">
-
-
-                                        <button className="text-lg text-[#0077FF] bg-[#BBDDFF] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <TbEdit />
-                                        </button>
-                                        <button className="text-lg text-[#F87171] bg-[#FEE2E2] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <RiDeleteBin6Line />
-                                        </button>
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b border-[#D0D2DA]">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    04
-                                </th>
-                                <td className="px-6 py-4">
-                                    Lorem ipsum dolor sit imet
-                                </td>
-                                <td className="px-6 py-4">
-                                    International
-                                </td>
-
-                                <td className="px-6 py-4">
-                                    <span className="flex items-center gap-3">
-
-
-                                        <button className="text-lg text-[#0077FF] bg-[#BBDDFF] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <TbEdit />
-                                        </button>
-                                        <button className="text-lg text-[#F87171] bg-[#FEE2E2] w-7  h-7 rounded-lg flex items-center justify-center">
-                                            <RiDeleteBin6Line />
-                                        </button>
-                                    </span>
-                                </td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
+        <form className="flex items-center justify-end text-right gap-3 mb-6">
+          <label for="simple-search" className="text-sm text-textColor">
+            Search
+          </label>
+          <div className="relative ">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <span className="text-xl text-textColor">
+                <CiSearch />
+              </span>
             </div>
+            <input
+              type="text"
+              className="bg-[#F0FDF4] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-2.5 py-3 border-none"
+              placeholder="Search"
+              required
+            />
+          </div>
+        </form>
 
-        </section>
-    )
-}
+        {/* medicine list table */}
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border border-[#d0d2dadd]  table_head rounded-lg">
+              <tr className="py-4 rounded-lg">
+                <th
+                  scope="col"
+                  className="px-6 py-3  text-[13px] font-medium capitalize"
+                >
+                  Seriol No
+                </th>
 
-export default AllBlogs
+                <th
+                  scope="col"
+                  className="px-6 py-3  text-[13px] font-medium capitalize"
+                >
+                  Title
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3  text-[13px] font-medium capitalize"
+                >
+                  Category
+                </th>
+
+                <th
+                  scope="col"
+                  className="px-6 py-3  text-[13px] font-medium capitalize"
+                >
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs?.map((blog, i) => (
+                <tr
+                  key={blog?._id}
+                  blog={blog}
+                  className="bg-white border-b border-[#D0D2DA]"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {i + 1}
+                  </th>
+                  <td className="px-6 py-4">{blog?.title}</td>
+                  <td className="px-6 py-4">{blog?.category}</td>
+
+                  <td className="px-6 py-4">
+                    <span className="flex items-center gap-3">
+                      <button className="text-lg text-[#0077FF] bg-[#BBDDFF] w-7  h-7 rounded-lg flex items-center justify-center">
+                        <TbEdit />
+                      </button>
+                      <button
+                        onClick={() => handelDelete(blog?._id)}
+                        className="text-lg text-[#F87171] bg-[#FEE2E2] w-7  h-7 rounded-lg flex items-center justify-center"
+                      >
+                        <RiDeleteBin6Line />
+                      </button>
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/* pagination */}
+      <Pagination
+        quantity={quantity}
+        page={page}
+        setPage={setPage}
+        size={size}
+        setSize={setSize}
+      />
+    </section>
+  );
+};
+
+export default AllBlogs;
