3a7d1fa38660726485bea2d592110a824c23f578
ifty03 <ashikulislamb84@gmail.com>
2023-06-06 21:21:02 +0600

    our best doctor page added

--- src/Components/Home/BestDoctors/BestDoctors.js
+++ src/Components/Home/BestDoctors/BestDoctors.js
@@ -1,88 +1,59 @@
-import React from 'react'
-import img1 from '../../../Assets/images/Dorctors/team-member-1.jpg'
-import img2 from '../../../Assets/images/Dorctors/team-member-2.jpg'
-import img3 from '../../../Assets/images/Dorctors/team-member-3.jpg'
-import img4 from '../../../Assets/images/Dorctors/team-member-4.jpg'
-import img5 from '../../../Assets/images/Dorctors/team-custom-icon-2.png'
-import img6 from '../../../Assets/images/Dorctors/team-custom-icon-1.png'
-import img7 from '../../../Assets/images/Dorctors/team-custom-icon-3.png'
-import img8 from '../../../Assets/images/Dorctors/team-custom-icon-4.png'
+import React from "react";
+import demoDoctor from "../../../Assets/images/doctor.jpg";
+import img7 from "../../../Assets/images/Dorctors/team-custom-icon-3.png";
 
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
+import { useEffect } from "react";
+import { useState } from "react";
 const BestDoctors = () => {
-
-    const doctorsData = [
-        {
-            _id: 1,
-            doctorsName: "Dr.Md.Jakiul Hasan",
-            category: "Private Practitioner",
-            description:"Bachelor Of Unani Medicine And Surgery(BUMS)",
-            // 
-            src:"https://api.huamc.gov.bd/uploads/photo/img-20221230-wa0019-1672409519387.jpg",
-            icon: `${img7}`,
-        },
-        {
-            _id: 2,
-            doctorsName: "Dr. Sudip Chandra Paul",
-            category: "General Practitioner & Nutritionist",
-            description:"MS in Food & Nutrition (Islamic University )",
-            // 
-            src:"https://api.huamc.gov.bd/uploads/photo/inbound5740222277292509008-1675621368082.jpg",
-            icon: `${img5}`,
-        },
-        {
-            _id: 3,
-            doctorsName: "Dr. Falguni Chakraborty",
-            category: "Private practice",
-            description:"B.U.M.S. (Govt. Unani and Ayurvedic Medical College & Hospital )",
-            price: "500",
-            // image: `${img3}`,
-            src:"https://api.huamc.gov.bd/uploads/photo/inbound922859787722436476-1671010728818.jpg",
-            icon: `${img6}`,
-        },
-        {
-            _id: 4,
-            doctorsName: "Dr.Md.Shariful Islam",
-            category: "Medical Officer,Uhc,Terokhada",
-            description:"Bachelor Of Unani Medicine &Surgery (BUMS) (University Of Dhaka)",
-            // image: `${img4}`,
-            src:"https://api.huamc.gov.bd/uploads/photo/1642612958335_copy_300x300_4-1671906608400.jpg",
-            icon: `${img8}`,
-        }
-      
-    ]
-    return (
-        <div className='lg:w-[90%] w-full mx-auto my-10 text-center '>
-            <h1 className='text-secondary font-semibold text-3xl mt-3'>Our Best Doctors</h1>
-            <p className='mt-3 text-textColor'>Comprehensive and Personalized Care for Your Well-being </p>
-            <div className=' flex gap-6  flex-wrap mx-auto justify-center  mt-14'>
-                {
-                    doctorsData.map((Data) => (
-
-                        <div className='w-56 mb-3' key={Data._id} relative>
-                            <div className=''>
-                                <img src={Data.icon} className='bg-primary w-12  h-12  rounded-full text-xs  ml-[150px] mt-4 text-white absolute'/>
-                                <img src={Data.src} alt='' className='w-36 h-36 rounded-full border-2 border-primary mx-auto mt-7' />
-                            </div>
-                            <div className='w-56'>
-                                <h1 className='text-secondary text-sm font-semibold mt-5'>{Data.doctorsName}</h1>
-                                <p className='text-lightPrimary text-xs mt-1'> {Data.category}</p>
-                                <p className='text-xs text-textColor mt-3 leading-relaxed'>{Data.description}</p>
-                            </div>
-
-                            <div className='flex mt-3 justify-center gap-2 h-4'>
-                            <Icon icon="uil:facebook-f" />
-                            <Icon icon="mdi:twitter" />
-                            <Icon icon="ri:linkedin-fill" />
-                            <Icon icon="mdi:youtube" />
-                            </div>
-                        </div>
-
-                    ))
-                }
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/api/v1/user`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data?.data);
+      });
+  }, []);
+  const doctors = user.filter((item) => item.role === "doctor");
+  console.log(doctors);
+  return (
+    <div className="lg:w-[90%] w-full mx-auto my-10 text-center ">
+      <h1 className="text-secondary font-semibold text-3xl mt-3">
+        Our Best Doctors
+      </h1>
+      <p className="mt-3 text-textColor">
+        Comprehensive and Personalized Care for Your Well-being{" "}
+      </p>
+      <div className=" flex gap-6  flex-wrap mx-auto justify-center  mt-14">
+        {doctors.map((Data) => (
+          <div className="w-56 mb-3" key={Data._id} relative>
+            <div className="">
+              <img
+                src={img7}
+                className="bg-primary w-12  h-12  rounded-full text-xs  ml-[150px] mt-4 text-white absolute"
+              />
+              <img
+                src={Data.img ? Data.img : demoDoctor}
+                alt=""
+                className="w-36 h-36 rounded-full border-2 border-primary mx-auto mt-7"
+              />
             </div>
-        </div>
-    )
-}
+            <div className="w-56">
+              <h1 className="text-secondary text-sm font-semibold mt-5">
+                {Data.fullName}
+              </h1>
+              <p className="text-lightPrimary text-xs mt-1">
+                {" "}
+                {Data.doctorDegree}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default BestDoctors
+export default BestDoctors;
--- src/Components/Home/NewArrivals/NewArrivals.js
+++ src/Components/Home/NewArrivals/NewArrivals.js
@@ -1,161 +1,108 @@
-import React from 'react'
-import img1 from "../../../Assets/images/NewArrival/16. Syrup Parsley.webp";
-import img2 from "../../../Assets/images/NewArrival/15. Capsul Ranix-Ginseng.webp";
-import img3 from "../../../Assets/images/NewArrival/14. Suspension Procid.webp";
-import img4 from "../../../Assets/images/NewArrival/09. S.Solid Dykmuni.webp";
-import img5 from "../../../Assets/images/NewArrival/07. Syrup Hazmina Plus.webp";
-import img6 from "../../../Assets/images/NewArrival/08. Syrup R-Kuli.webp";
-import Slider from 'react-slick'
-import { TbCurrencyTaka } from "react-icons/tb"
-import { Link } from 'react-router-dom';
+import React from "react";
 
+import Slider from "react-slick";
+import { TbCurrencyTaka } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const NewArrivals = () => {
-    var settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/api/v1/medicine?size=${6}&page=${0}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data?.data);
+      });
+  }, []);
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
 
-        slidesToScroll: 1,
-        autoplay: true,
-        arrows: true,
-        autoplaySpeed: 1500,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-        // fade: true,
-    };
-
-
-    const newProducts = [
-        {
-            _id: 1,
-            productName: "Syrup Parsley",
-            genericName: "Arq Bukhar",
-            strength: "200 ML / 100 ML",
-            Catgory:"Syrup",
-
-            stock: "300",
-            price: "145/75",
-            image: `${img1}`,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
         },
-        {
-            _id: 2,
-            productName: "Capsul Renix-Ginseng",
-            genericName: "Ginseng",
-            strength: "60 Capsule",
-            Catgory:"Capsule",
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    // fade: true,
+  };
 
-            stock: "90",
-            price: "1200",
-            image: `${img2}`,
-        },
-        {
-            _id: 3,
-            productName: "Suspension Procid",
-            genericName: "Humuzin",
-            strength: "20 ML",
-            Catgory:"Suspension",
-            stock: "180",
-            price: "220",
-            image: `${img3}`,
-        },
-        {
-            _id: 4,
-            productName: "S.Solid Dykmuni",
-            genericName: "Jowarish Kamuni",
-            strength: "400 G / 100 G",
-            Catgory:"S.solid",
-            stock: "160",
-            price: "350/250",
-            image: `${img4}`,
-        },
-        {
-            _id: 5,
-            productName: "Syrup R-Khuli",
-            genericName: "Urinary Alkalinizer & Diuretic",
-            Catgory:"Syrup",
-            strength: "450 ML/200 ML/100 ML",
-            stock: "80",
-            price: "225/115/60",
-            image: `${img6}`,
-        },
-        {
-            _id: 6,
-            productName: "Syrup Hazmina-Plus",
-            genericName: " Sharbat Hazmina",
-            strength: "450 ML/200 ML",
-            Catgory:"Syrup",
-            stock: "200",
-            price: "225/115",
-            image: `${img5}`,
-        },
-    ];
-    return (
-        <div className=' w-full py-14 bg-[#f7fbf3]'>
-            <div className="lg:w-[90%] w-full  mx-auto my-10 text-center">
-                <h3 className='bg-thirdLightPrimary w-36 mx-auto font '>F e a t u r e</h3>
-                <h1 className='text-secondary font-semibold text-3xl mt-3 mb-6'>New Arrivals</h1>
-                <Slider {...settings}>
-                    {
-                        newProducts?.slice(0, 4)?.map((product) => (
-
-                            <div className='w-80 sm:w-6/12 md:w-4/12' key={product._id}>
-                                <div className='m-4 w-50 rounded-xl border-2 border-whiteSmoke '>
-                                    <h2 className='bg-primary w-10 text-left text-xs pl-1 pt-1 pb-1  text-white mt-3'>Sale!</h2>
-                                    <img src={product.image} alt='' className='w-full mx-auto  h-auto mt-7' />
-                                    <div className='pl-4 pb-2' >
-                                    <h1 className='text-secondary text-left  text-lg mt-2'>{product.productName}</h1>
-                                    <div className="flex  pb-2  items-center text-left  ">
-                                    <span className="text-lg mt-2 mr-2">
-                                        <TbCurrencyTaka ></TbCurrencyTaka>
-                                    </span>
-                                    <p className=' text-primary text-lg mt-2'> {product.price}</p>
-                                    
-                                </div> 
-                                <Link to="/checkout" ><button   className= ' bg-primary px-2 pt-1 pb-1 rounded-lg text-left flex bottom-0 left-0' >Order Now</button></Link>
-                                
-
-                                </div>
-                                </div>
-                                
-                            </div>
-
-                        ))
-                    }
-                </Slider>
+  return (
+    <div className=" w-full py-14 bg-[#f7fbf3]">
+      <div className="lg:w-[90%] w-full  mx-auto my-10 text-center">
+        <h3 className="bg-thirdLightPrimary w-36 mx-auto font ">
+          F e a t u r e
+        </h3>
+        <h1 className="text-secondary font-semibold text-3xl mt-3 mb-6">
+          New Arrivals
+        </h1>
+        <Slider {...settings}>
+          {products?.map((product) => (
+            <div className="w-80 sm:w-6/12 md:w-4/12 " key={product._id}>
+              <div className="m-4 w-50 rounded-xl border-2 border-whiteSmoke ">
+                <h2 className="bg-primary w-10 text-left text-xs pl-1 pt-1 pb-1  text-white mt-3">
+                  Sale!
+                </h2>
+                <img
+                  src={product.img}
+                  alt=""
+                  className="w-full mx-auto  h-auto mt-7"
+                />
+                <div className="pl-4 pb-2">
+                  <h1 className="text-secondary text-left  text-lg mt-2">
+                    {product.productName}
+                  </h1>
+                  <div className="flex  pb-2  items-center text-left  ">
+                    <span className="text-lg mt-2 mr-2">
+                      <TbCurrencyTaka></TbCurrencyTaka>
+                    </span>
+                    <p className=" text-primary text-lg mt-2">
+                      {" "}
+                      {product.price}
+                    </p>
+                  </div>
+                  <Link to={`/products/${product?._id}`}>
+                    <button className=" bg-primary px-2 pt-1 pb-1 rounded-lg text-left flex bottom-0 left-0">
+                      Order Now
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-        </div>
-    )
-}
-
-export default NewArrivals
+export default NewArrivals;
